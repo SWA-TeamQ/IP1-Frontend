920953c9e461d0f7b843c8e31dedeffc386e5c75
Abel Mekonnen <abelmekonen247@gmail.com>
2025-12-16 02:17:45 +0300

    refactor: clean up unused styles and scripts; comment out deprecated imports and remove unnecessary files

--- utils/urls.js
+++
@@ -1,5 +0,0 @@
-// utils/urls.js
-// Removed: project-root URL helper functions.
-// Paths are now hard-coded throughout the project (assets/pages/etc).
-// This file intentionally left minimal to avoid accidental usage.
-// If you want this file fully deleted from disk, remove it manually from the project.
