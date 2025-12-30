547ccc8c679e66a6a9ef168d64a3b0853c1cb796
Barok-y <barokyeshibr@gmail.com>
2025-12-30 16:40:47 +0300

    Refactor on Product Detail

--- js/classes/product.js
+++ js/classes/product.js
@@ -39,4 +39,10 @@ class Product {
     }
 }
 
-export { Product, ProductDetails };
+
+function changeImage(el){
+    document.querySelectorAll('.thumbnail-gallery img').forEach(i=>i.classList.remove('active'));
+    el.classList.add('active');
+    mainImg.style.opacity=0;
+    setTimeout(()=>{mainImg.src=el.src;mainImg.style.transform='scale(1.02)';mainImg.style.opacity=1;setTimeout(()=>mainImg.style.transform='scale(1)',200);},200);
+}
