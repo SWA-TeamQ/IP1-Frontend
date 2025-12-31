8d0398653cd347be088caf87a0feec4d82e159cc
Abel Mekonnen <abelmekonen247@gmail.com>
2026-01-01 00:04:22 +0300

    feat: implement image gallery functionality on product detail page; refactor styles and scripts

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { $ } from "/src/utils/dom.js";
 import { initApp } from "./scripts/initApp.js";
 import { initHomePage } from "./scripts/home.page.js";
 import { initProductsPage } from "./scripts/products.page.js";
@@ -9,3 +10,20 @@ initHomePage();
 initProductsPage();
 initProductDetailPage();
 initCheckout();
+
+function changeImage(el) {
+    const mainImg = $(".main-image");
+    document
+        .querySelectorAll(".thumbnail-gallery img")
+        .forEach((i) => i.classList.remove("active"));
+    el.classList.add("active");
+    mainImg.style.opacity = 0;
+    setTimeout(() => {
+        mainImg.src = el.src;
+        mainImg.style.transform = "scale(1.02)";
+        mainImg.style.opacity = 1;
+        setTimeout(() => (mainImg.style.transform = "scale(1)"), 200);
+    }, 200);
+}
+
+window.changeImage = changeImage;
